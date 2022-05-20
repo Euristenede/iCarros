55180f144b9fd086713b5637ed88c47c2bc34219
Euristenede <euristenedesantos@gmail.com>
2022-05-19 21:11:19 -0300

    Adicionado condição if para médias de alunos

--- workspace/CalculadoraMedia/src/calculadoramedia/Media.java
+++ workspace/CalculadoraMedia/src/calculadoramedia/Media.java
@@ -14,12 +14,12 @@ public class Media {
     public static void main(String[] args) {
         String nomeAluno;
         Float nota1, nota2, nota3, nota4, media;
-        
+
         Scanner teclado = new Scanner(System.in);
-        
+
         System.out.println("Digite o nome do aluno: ");
         nomeAluno = teclado.nextLine();
-        
+
         System.out.println("Digita a nota 1: ");
         nota1 = teclado.nextFloat();
         System.out.println("Digita a nota 2: ");
@@ -28,10 +28,19 @@ public class Media {
         nota3 = teclado.nextFloat();
         System.out.println("Digita a nota 4: ");
         nota4 = teclado.nextFloat();
+
+        media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("Olá " + nomeAluno + " suas notas são " + nota1 + ", " + nota2 + ", " + nota3 + ", " + nota4 + " com uma média de: " + media);
         
-        media = (nota1+nota2+nota3+nota4)/4;
+        if(media < 5){
+            System.out.println("Aluno reprovado");
+        }else if(media == 5){
+            System.out.println("Aluno em recuperação");
+        }else{
+            System.out.println("Aluno aprovado");
+        }
         
-        System.out.println("Olá "+nomeAluno+" suas notas são "+nota1+", "+nota2+", "+nota3+", "+nota4+" com uma média de: "+media);
     }
-    
+
 }
